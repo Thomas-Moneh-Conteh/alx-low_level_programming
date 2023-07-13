e2038e17775ad23be5f9edad9fda4bc2afda0ebf
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-07-13 15:41:44 -0700

    sizeof function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %zu\n", sizeof(char));
-	printf("Size of an int: %zu\n", sizeof(int));
-	printf("Size of a long int: %zu\n", sizeof(long int));
-	printf("Size of a long long int: %zu\n", sizeof(long long int));
-	printf("Size of a float: %zu\n", sizeof(float));
+	printf("Size of a char: %zu\n", sizeof(char) "byte(s)");
+	printf("Size of an int: %zu\n", sizeof(int) "byte(s)");
+	printf("Size of a long int: %zu\n", sizeof(long int) "byte(s)");
+	printf("Size of a long long int: %zu\n", sizeof(long long int) "byte(s)");
+	printf("Size of a float: %zu\n", sizeof(float) "byte(s)");
 	return (0);
 }
