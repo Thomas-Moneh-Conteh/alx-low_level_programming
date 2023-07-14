db1d21f7d12cc125b228e5361469ce56ff12d6f8
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-07-14 16:57:39 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This will output the alphabets in ascending order
+ * return: returns 0
+ */
+
+int main(void)
+{
+	char myLet;
+
+	for (myLet = 'a'; <= 'z'; myLet++)
+	{
+		putchar(myLet);
+		putchar(' ');
+	}
+	return (0);
+}
