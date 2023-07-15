af989381e794a795b72e735f6666d003db011b04
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-07-15 02:59:13 -0700

    The alphabet in lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - This will output the alphabets in ascending order
+ * main - Entry point
+ * Description: This program prints the alphabet from 'A' to 'Z'
+ *              using two putchar calls without spacing between the letters,
+ *              and outputs a newline character at the end.
  * return: returns 0
  */
 
@@ -12,7 +15,7 @@ int main(void)
 	for (myLet = 'a'; myLet <= 'z'; myLet++)
 	{
 		putchar(myLet);
-		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
