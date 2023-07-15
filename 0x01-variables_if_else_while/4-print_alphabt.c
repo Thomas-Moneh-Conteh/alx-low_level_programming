32c4aa13df9f1af7e8f74cb9144e681f94020afb
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-07-15 12:17:54 -0700

    not q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints all the alphabet expect q and e
+ * in lowercase followed by a newline.
+ * return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'q' && a != 'e')
+			putchar(a);
+		a++;
+	}
+	putchar ('\n);
+	return (0);
+}
