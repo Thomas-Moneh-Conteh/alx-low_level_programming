85f0ea3437665e614dc053e504d96ec9441d882c
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-07-15 17:08:03 -0700

    comma and spacing numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+
+		if (i != 9)
+
+		putchar(',');
+
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
