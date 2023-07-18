cbf70c8841c0d70b5ced7d1153cc954a21d54a09
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-07-18 15:41:50 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _isalpha - main entry
+ * @c: tested characted
+ * Return: always 0.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
