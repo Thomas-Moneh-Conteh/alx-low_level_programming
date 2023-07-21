8e9031f6a819a921639e1afd365bd70596f19f9d
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-07-21 15:09:18 -0700

    FizzBuzz game

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Check the code.
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+		printf("%s", "FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+		printf("%s", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+		printf("%s", "Buzz");
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+		if (i != 100)
+		{
+		printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
