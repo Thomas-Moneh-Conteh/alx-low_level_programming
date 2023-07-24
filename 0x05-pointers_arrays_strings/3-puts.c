5f00aca64ad7aac0f166dbf19668421d140a85e2
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-07-24 16:31:35 -0700

    Using fputs to print to the stdout instead of puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - A function that prints a string,
+ * followed by a new line, to stdout.
+ * @str: string to be printed.
+ *
+ * Return: always 0.
+ */
+
+void _puts(char *str)
+{
+	fputs(str, stdout);
+}
