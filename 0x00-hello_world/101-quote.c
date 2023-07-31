54cd93c7c98308ee955cfac4550f30c735e9db6c
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-07-31 14:10:42 -0700

    Hot fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,11 @@
  * Return: This return 1
  */
 
+
 int main(void)
 {
-	putchar("and that piece of art is useful" - Dora Korpar, "2015-10-19");
+	char message[] = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, message, sizeof(message) - 1);
 	return (1);
 }
+
