1c8bc0824e5e3b7d1dcb4a4220f46bf6a551c6fa
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-08-04 17:19:42 -0700

    fix

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -9,16 +9,15 @@
 
 char *_strcpy(char *dest, char *src)
 {
-        int i, length;
+	int i, length;
 
-        for (length = 0; src[length] != '\0'; length++)
-        {
-        }
+	for (length = 0; src[length] != '\0'; length++)
+	{
+	}
 
-        for (i = 0; i <= length ; i++)
-        {
-                dest[i] = src[i];
-        }
-        return (dest);
+	for (i = 0; i <= length ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
-
