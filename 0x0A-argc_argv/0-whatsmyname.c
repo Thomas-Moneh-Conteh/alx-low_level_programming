2a9a175481d12eaef6ae2424288ff1b55fdb1252
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-08-06 10:54:19 -0700

    Print myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main entry point
+ * 
+ * Return: always 0.
+ */
+int main(int argc, char *agrv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
