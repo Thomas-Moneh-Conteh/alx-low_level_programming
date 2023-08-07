4e9d7da1b37011c8c631283deec6f5b7bd0adf77
Thomas-Moneh-Conteh <monehconteh40824@gmail.com>
2023-08-07 15:55:09 -0700

    A function that create an array of cars and initializes with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - A function that creates an array
+ * of chars, and initializes it with a specific char.
+ * @size: size of integer
+ * @c: character to be created
+ *
+ * Return: always nothing
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(size * sizeof(arr));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
+	return (arr);
+
+}
